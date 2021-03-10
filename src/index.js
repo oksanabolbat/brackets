b53eb3744c4252d85f5d69756f462aaa5ef5815d
oksanabolbat <oksana.bolbat@gmail.com>
2021-03-10 20:36:47 +0200

    init: function created

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    // your solution
+    let bracketsPairs = [];
+    bracketsPairs = bracketsConfig.map((arr) => arr[0] + arr[1]);
+    let j = 0;
+    let prev = 0;
+    while (prev != str.length) {
+        prev = str.length;
+        for (let i = 0; i < bracketsPairs.length; i++) {
+            str = str.replace(bracketsPairs[i], "");
+        }
+        if (str.length === 0) return true;
+    }
+
+    return false;
+};
